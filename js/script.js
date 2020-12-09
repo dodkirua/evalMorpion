e125c6af691ca3ddada729022418da9f1e88a6d2
Dodkirua <73292390+dodkirua@users.noreply.github.com>
2020-12-09 14:28:35 +0100

    push branch beer

--- js/script.js
+++ js/script.js
@@ -3,29 +3,58 @@ let box = document.getElementById("grid").getElementsByTagName("div");
 let button = document.getElementsByTagName("button");
 let token = 0;
 let altPlayer = 0;
+let secret = 0;
 
 document.addEventListener('contextmenu', function (event){
     event.preventDefault();
 });
 
+button[2].addEventListener("dblclick", function (){
+    secret = 1;
+})
+
+button[0].addEventListener("dblclick", function (){
+    secret = 0;
+})
+
 for (let i = 0 ; i < box.length ; i++)
 {
     box[i].addEventListener("contextmenu", function (){
         if (token === 0){
-            player2(i);
+            if (secret === 0){
+                player2(i);
+            }
+            else {
+                player2Secret(i);
+            }
         }
 
     });
 
    box[i].addEventListener("click", function (){
         if (altPlayer === 0){
-            player1(i);
+            if (secret === 0){
+                player1(i);
+            }
+            else {
+                player1Secret(i);
+            }
         }
         else{
-            player2(i);
+            if (secret === 0){
+                player2(i);
+            }
+            else {
+                player2Secret(i);
+            }
         }
         if (token === 1){
-            botPlay();
+            if (secret === 0){
+                botPlay(i);
+            }
+            else {
+                botPlaySecret(i);
+            }
         }
     });
 }
@@ -36,12 +65,10 @@ for (let i = 0 ; i < button.length ; i++)
         switch (this.innerHTML){
             case "Human":
                 token = 0;
-                console.log(token);
                 restart();
                 break;
             case "Bot":
                 token = 1;
-                console.log(token);
                 restart();
                 break;
             case "Restart" :
@@ -58,6 +85,7 @@ function restart() {
     {
         box[i].removeAttribute("id");
         box[i].removeAttribute("style");
+        box[i].innerHTML = "";
         altPlayer = 0;
     }
 }
@@ -107,6 +135,14 @@ function winner(win) {
             alert("Match nul");
             restart();
             break;
+        case "beer":
+            alert("Bravo Joueur 1 tu paie une bière au joueur 2");
+            restart();
+            break;
+        case "coffee":
+            alert("Bravo Joueur 2 tu paie un café au joueur 1");
+            restart();
+            break;
     }
 }
 
@@ -116,6 +152,11 @@ function botPlay(){
     }
 }
 
+function botPlaySecret(){
+    for (let i = 0 ; i < box.length ; i++){
+        player2Secret(i);
+    }
+}
 function player2 (i){
     if (altPlayer === 1) {
         if (box[i].id === "") {
@@ -132,6 +173,30 @@ function player1 (i){
         if (box[i].id === ""){
             box[i].id = "cross";
             box[i].style.backgroundColor = "#009d0e";
+            testVictory();
+            altPlayer = 1;
+        }
+    }
+}
+
+function player2Secret (i){
+    if (altPlayer === 1) {
+        if (box[i].id === "") {
+            box[i].id = "coffee";
+            box[i].style.backgroundColor = "#085394";
+            box[i].innerHTML = "<i class=\"fas fa-coffee\"></i>"
+            testVictory();
+            altPlayer = 0;
+        }
+    }
+}
+
+function player1Secret (i){
+    if (altPlayer === 0){
+        if (box[i].id === ""){
+            box[i].id = "Beer";
+            box[i].style.backgroundColor = "#009d0e";
+            box[i].innerHTML = "<i class=\"fas fa-beer\"></i>"
             testVictory();
             altPlayer = 1;
         }
