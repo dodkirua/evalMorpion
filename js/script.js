09d1c96db43c0ca728ebce5b4a3db8b3f8fe7260
Dodkirua <73292390+dodkirua@users.noreply.github.com>
2020-12-09 13:37:16 +0100

    reduction du nombre de ligne et retrait du log

--- js/script.js
+++ js/script.js
@@ -3,23 +3,6 @@ let box = document.getElementById("grid").getElementsByTagName("div");
 let button = document.getElementsByTagName("button");
 let token = 0;
 let altPlayer = 0;
-console.log(box[0]);
-
-
-
-for (let i = 0 ; i < box.length ; i++)
-{
-    box[i].addEventListener("click", function (){
-        if (altPlayer === 0){
-            if (this.id === ""){
-                this.id = "cross";
-                this.style.backgroundColor = "#009d0e";
-                testVictory();
-            }
-            altPlayer = 1;
-        }
-    });
-}
 
 document.addEventListener('contextmenu', function (event){
     event.preventDefault();
@@ -28,28 +11,15 @@ document.addEventListener('contextmenu', function (event){
 for (let i = 0 ; i < box.length ; i++)
 {
     box[i].addEventListener("contextmenu", function (){
-        if (altPlayer === 1) {
-            if (this.id === "") {
-                this.id = "round";
-                this.style.backgroundColor = "#085394";
-                testVictory();
-            }
-            altPlayer = 0;
-        }
+        player2(i);
     });
-}
 
-for (let i = 0 ; i < box.length ; i++)
-{
     box[i].addEventListener("dblclick", function (){
-        if (altPlayer === 1) {
-            if (this.id === "") {
-                this.id = "round";
-                this.style.backgroundColor = "#085394";
-                testVictory();
-            }
-            altPlayer = 0;
-        }
+        player2(i);
+    });
+
+    box[i].addEventListener("click", function (){
+        player1(i);
     });
 }
 
@@ -85,30 +55,24 @@ function restart() {
 
 function testVictory(){
 
-    if (box[0].id === box[1].id && box[0].id ===box[2].id){
-        winner(box[0].id);
+    for (let i = 0 ; i < 9 ; i = i+3 ){
+        if (box[i].id === box[i+1].id && box[i].id ===box[i+2].id){
+            winner(box[i].id);
+        }
     }
-    if(box[3].id === box[4].id && box[3].id === box[5].id){
-        winner(box[3].id);
+    for (let i = 0 ; i < 3 ;  i++ ){
+        if (box[i].id === box[i+3].id && box[i].id ===box[i+6].id){
+            winner(box[i].id);
+        }
     }
-    if(box[6].id === box[7].id && box[6].id ===box[8].id){
-        winner(box[6].id);
-    }
+
     if(box[0].id === box[4].id && box[0].id ===box[8].id){
         winner(box[0].id);
     }
     if(box[2].id === box[4].id && box[2].id ===box[6].id){
         winner(box[2].id);
     }
-    if(box[0].id === box[3].id && box[0].id ===box[6].id){
-        winner(box[0].id);
-    }
-    if(box[1].id === box[4].id && box[1].id ===box[7].id){
-        winner(box[1].id);
-    }
-    if(box[2].id === box[5].id && box[2].id ===box[8].id){
-        winner(box[2].id);
-    }
+
     let nbId = 0;
     for (let i = 0 ; i < box.length ; i++) {
         if (box[i].id != ""){
@@ -134,5 +98,35 @@ function winner(win) {
             alert("Match nul");
             restart();
             break;
+    }
+}
+
+function botPlay(i){
+    switch (box[i].id){
+        case "":
+
+    }
+}
+
+function player2 (i){
+    if (altPlayer === 1) {
+        if (box[i].id === "") {
+            box[i].id = "round";
+            box[i].style.backgroundColor = "#085394";
+            testVictory();
+            altPlayer = 0;
+        }
+    }
+}
+
+function player1 (i){
+    if (altPlayer === 0){
+        if (box[i].id === ""){
+            box[i].id = "cross";
+            box[i].style.backgroundColor = "#009d0e";
+            testVictory();
+            altPlayer = 1;
+        }
+
     }
 }
